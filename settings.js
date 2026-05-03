aa6772921951dc7a0f2250e7a3f800c3ce51606d
buntyrdp <smartykhan484@gmail.com>
2026-05-03 05:29:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,20 +1,22 @@
-const settings = {
-  OWNER_NUMBER: 923073022765
-    OWNER_NAME: "Bunty"
-BOT_NAME: "monarch"
-  timezone: 'Africa/Accra',
-  prefix: '.',
-  ownerNumber: '', 923432443022
-  AUTO_STATUS_REACT: 'false',
-  AUTO_STATUS_REPLY: 'false',
-  AUTO_STATUS_MSG: 'Status Viewed Queen Riam',
-  AUTORECORD: 'false',
-  AUTOTYPE: 'false',
-  AUTORECORDTYPE: 'false',
-  giphyApiKey: '',
-  commandMode: 'public',
-  description: 'This is a bot for managing group commands and automating tasks.',
-  version: '2.0.0',
-};
+const fs = require('fs')
+const chalk = require('chalk')
 
-module.exports = settings;
+// Bunty Bhai's Settings
+global.OWNER_NUMBER = '923073022765'
+global.OWNER_NAME = 'Bunty'
+global.BOT_NAME = 'Bunty-Bot'
+global.PREFIX = '.'
+global.TIME_ZONE = 'Asia/Karachi'
+
+// Baki settings default
+global.AUTO_REACTION = false
+global.AUTO_TYPING = false
+global.AUTO_RECORDING = false
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
